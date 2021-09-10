93f5e01ffb286204b35c74b5c0e5e290d4da2f63
Enzo CONTY <indefined3@gmail.com>
2021-09-10 19:24:47 +0200

    :lipstick: maintability

--- TwitchHueControlDesktop/lib/main.dart
+++ TwitchHueControlDesktop/lib/main.dart
@@ -1,76 +1,14 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Twitch Hue Control',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HueApplication(),
-    );
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Twitch Hue Control');
+    setWindowMinSize(const Size(1100, 700));
+    setWindowMaxSize(Size.infinite);
   }
-}
-
-class HueApplication extends StatefulWidget {
-  const HueApplication({Key? key}) : super(key: key);
-
-  @override
-  State<HueApplication> createState() => _HueApplicationState();
-}
-
-class _HueApplicationState extends State<HueApplication> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello"),
-      ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.green,
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
+  runApp(const Application());
 }
---
+++ TwitchHueControlDesktop/lib/misc/k_config.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kAppPurple = Color.fromRGBO(171, 51, 176, 1);
+const kAppBlue = Color.fromRGBO(77, 140, 194, 1);
+const kAppInput = Color.fromRGBO(248, 248, 248, 1);
+
+const kBasicGradient = LinearGradient(
+  begin: Alignment.centerRight,
+  end: Alignment.centerLeft,
+  colors: [kAppPurple, kAppBlue],
+);
